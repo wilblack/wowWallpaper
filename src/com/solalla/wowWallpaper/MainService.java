eb199d525e537215637db46182f41796da43a557
wilblack <wilblack21@gmail.com>
2012-10-20 18:12:07 -0700

    Added not found and local storage of image.

--- src/com/solalla/wowWallpaper/MainService.java
+++ src/com/solalla/wowWallpaper/MainService.java
@@ -17,9 +17,13 @@
 package com.solalla.wowWallpaper;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Date;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -36,17 +40,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Handler;
-import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.service.wallpaper.WallpaperService;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 
 /*
@@ -56,7 +56,8 @@ import android.view.SurfaceHolder;
  */
 public class MainService extends WallpaperService {
 	static String TAG = "ImageEngine";
-	
+	String lastImageName = "last-image.png";
+		
 	int updateFreqLong = 6*60*60*1000;
 	int updateFreqShort = 60*60*1000;
 	
@@ -79,11 +80,9 @@ public class MainService extends WallpaperService {
     }
    
     class ImageEngine extends Engine {
-    	
-    	//following line I want change to ImageView
-        Bitmap pic = BitmapFactory.decodeResource(getResources(), R.drawable.coiler);
+    	   	       
         private boolean mVisible = false;
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainService.this);
+        SharedPreferences prefs;
                
         private final Handler mHandler = new Handler();
         private final Runnable mUpdateDisplay = new Runnable() {
@@ -91,26 +90,55 @@ public class MainService extends WallpaperService {
         };
                 
         void draw(){
+        	Log.d(TAG , "[draw()]");
         	
-        	if (!haveNetwork()){
-        		mHandler.postDelayed(mUpdateDisplay, updateFreqShort);
-        		return;
+        	prefs = PreferenceManager.getDefaultSharedPreferences(MainService.this);
+        	       	        	
+        	long lastUpdated = prefs.getLong("lastUpdated", (long) 0);
+        	boolean refresh = prefs.getBoolean("refresh", false);
+        	Log.d(TAG, "refresh:" + refresh);
+        	
+        	boolean needUpdate = false;       	
+        	long now = new Date().getTime();
+        	
+        	
+        	Bitmap rawBgImage = null;
+        	Bitmap bgImage;
+        	
+        	SurfaceHolder holder = getSurfaceHolder();
+        	Canvas c = holder.lockCanvas();
+        	
+        	// Determine if we need an update based on whether we have a stored image or its out of date
+        	if (lastUpdated == (long) 0 || now - lastUpdated > updateFreqLong || refresh) {
+        		needUpdate = true;
+        		prefs.edit().putBoolean("refresh", false).commit();
+        	        		
         	}
         	
-        	Bitmap rawBgImage = getImage();      	
-        	SurfaceHolder holder = getSurfaceHolder();
-            Canvas c = null;
-            try {
-                c = holder.lockCanvas();
-                if (c != null) {
+        	Log.d(TAG, "needUpdate: " + needUpdate);
+        	
+        	if (haveNetwork() && needUpdate){
+        		// We have a network and we need an update so fetch and scale image
+        		rawBgImage = fetchImage();
+        	} else {
+        		// Get the old image        		
+        		rawBgImage = getLocalImage();	
+            }
+        	bgImage = scaleImage(rawBgImage, c);
+        	// Final catch
+        	if (bgImage == null) {
+        		Log.d(TAG,"Could not find an image. Using default");
+        		rawBgImage = BitmapFactory.decodeResource(getResources(), R.drawable.not_found);
+        		bgImage = scaleImage(rawBgImage, c);
+        	}
+        	
+        	try {
+        		if (c != null) {
                     //paint black
                     Paint p = new Paint();
                     p.setColor(Color.BLACK);
                     c.drawRect(0, 0, c.getWidth(), c.getHeight(), p);
-                    
-                    //Scale the image
-                    Bitmap bgImage = scaleImage(rawBgImage, c);
-                    
+                                       
                     // Draw the background image
                     BitmapFactory.Options options = new BitmapFactory.Options();
                     options.inPurgeable = true;                   
@@ -123,8 +151,17 @@ public class MainService extends WallpaperService {
             mHandler.postDelayed(mUpdateDisplay, updateFreqLong);
             
         }
-                
-        Bitmap getImage(){
+         
+        Bitmap getLocalImage(){
+        	Bitmap img = null;
+        	
+        	File dir = MainService.this.getFilesDir();
+        	Log.d(TAG,"Trying to load: " + dir + "/"+ lastImageName);
+        	img = BitmapFactory.decodeFile(dir + "/"+ lastImageName);
+        	return img;
+        }
+        
+        Bitmap fetchImage(){
         	Bitmap img = null;
         	
         	String imageUrl = null;
@@ -145,23 +182,47 @@ public class MainService extends WallpaperService {
 			} catch (JSONException e) {
 				Log.d(TAG,"Could not parse JSON from WoW "+e.toString());
 			}
-        	//Log.d(TAG, "imageUrl: " +imageUrl);
+        	Log.d(TAG, "imageUrl: " +imageUrl);
         	        	
         	if (imageUrl == null){
-        		img =  BitmapFactory.decodeResource(getResources(), R.drawable.coiler);
+        		img =  BitmapFactory.decodeResource(getResources(), R.drawable.not_found);
         	
         	} else {
         		try {
 					img = fetchImage(imageUrl);
 				} catch (ClientProtocolException e) {
 					Log.d(TAG,"Failed to execute httpclient"+e.toString());
-					img =  BitmapFactory.decodeResource(getResources(), R.drawable.coiler);
+					img =  BitmapFactory.decodeResource(getResources(), R.drawable.not_found);
 				} catch (IOException e) {
 					Log.d(TAG,"Failed to get the image from the response"+e.toString());
-					img =  BitmapFactory.decodeResource(getResources(), R.drawable.coiler);
+					img =  BitmapFactory.decodeResource(getResources(), R.drawable.not_found);
 				}
         	}
-        	       	
+        	if (img != null){
+        		
+        		// Save locally
+        		FileOutputStream fos = null;
+        		try {
+					fos = openFileOutput(lastImageName, MainService.this.MODE_PRIVATE);					
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					Log.e(TAG,"Could not find image file: "+ lastImageName);
+					Log.e(TAG,e.toString());
+				}
+        		
+        		if (fos != null){
+	        		img.compress(Bitmap.CompressFormat.PNG, 100, fos);  
+					try {
+						fos.close();
+					} catch (IOException e) {
+						Log.e(TAG,"Could not write file" + e.toString());
+					}
+        		}
+        		
+        		// Update timestamp
+        		prefs.edit().putLong("lastUpdated", new Date().getTime()).commit();
+        	}
+        	
         	return img;
         }
                 
@@ -312,7 +373,7 @@ public class MainService extends WallpaperService {
 	            if (info != null) {
 	                for (int i = 0; i < info.length; i++) {
 	                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
-	                    	Log.d(TAG, "Connected to network");	
+	                    	Log.d(TAG, "You are connected to a network");	
 	                    	return true;
 	                    }
 	                }
